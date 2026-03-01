8081b3ad9be3694ae176d50975cd1ee8c345805e
Dasia Mitchell <dmit543@wgu.edu>
2026-02-28 20:06:04 -0600

    Connect Supabase portal

--- portal-config.js
+++ portal-config.js
@@ -1,4 +1,4 @@
-export const SUPABASE_URL = "";
-export const SUPABASE_ANON_KEY = "";
+export const SUPABASE_URL = "https://labyrjsibcbcjbgesxyy.supabase.co";
+export const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxhYnlyanNpYmNiY2piZ2VzeHl5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzIzMjU5NzksImV4cCI6MjA4NzkwMTk3OX0.QI2KEg-8tgDR4DKN83HofYPREpFmet2T7q3LlZRWNTQ";
 export const PORTAL_BRAND = "The Legacy Group LLC";
 export const APP_ORIGIN = window.location.origin;
